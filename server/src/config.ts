b8c394ab26d5c9c625b91a1a9c8c802dddbf52f3
Evgeniy Shlenskiy <shlenskiyevgeniy@gmail.com>
2024-05-20 14:43:23 +0300

    deploying

--- server/src/config.ts
+++ server/src/config.ts
@@ -28,7 +28,10 @@ export const SMTP_HOST = process.env.SMTP_HOST as string;
 export const SMTP_PORT = Number(process.env.SMTP_PORT as string);
 export const SMTP_USER = process.env.SMTP_USER as string;
 export const SMTP_PASSWORD = process.env.SMTP_PASSWORD as string;
-export const API_URL = `http://localhost:${PORT}`;
+export const API_URL =
+    NODE_ENV === "production"
+        ? "https://films-production.up.railway.app"
+        : `http://localhost:${PORT}`;
 export const CLIENT_URL = `http://localhost:8080`;
 export const BCRYPT_SALT_ROUNDS = 10;
 export const EMAIL_FROM = "Rest Api Test";
