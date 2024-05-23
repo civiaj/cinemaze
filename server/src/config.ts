d0a96a502efc94cc18b3941ab4333e11a6f880f4
Evgeniy Shlenskiy <shlenskiyevgeniy@gmail.com>
2024-05-23 18:09:49 +0300

    deploy configurations

--- server/src/config.ts
+++ server/src/config.ts
@@ -29,7 +29,8 @@ export const SMTP_PORT = Number(process.env.SMTP_PORT as string);
 export const SMTP_USER = process.env.SMTP_USER as string;
 export const SMTP_PASSWORD = process.env.SMTP_PASSWORD as string;
 export const API_URL = `http://localhost:${PORT}`;
-export const CLIENT_URL = NODE_ENV === "production" ? "http://localhost" : "http://localhost:8080";
+export const CLIENT_URL =
+    NODE_ENV === "production" ? "http://localhost:80" : "http://localhost:8080";
 export const BCRYPT_SALT_ROUNDS = 10;
 export const EMAIL_FROM = "Rest Api Test";
 
